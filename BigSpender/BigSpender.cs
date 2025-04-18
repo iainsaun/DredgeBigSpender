c3b5a294c37e2e17bd5882792c70f844fada2ff7
iainsaun <iainsaun@desktop>
2025-04-18 09:36:36 +0100

    Literally just a comment in the main file

--- BigSpender/BigSpender.cs
+++ BigSpender/BigSpender.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using Winch.Core;
-
+/// <summary>
+/// This is the place to write the code, also, my first comment.
+/// </summary>
 namespace BigSpender
 {
 	public class BigSpender : MonoBehaviour
