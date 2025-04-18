49dae1ade254dfbc979090ea42754efa150ca5d5
iainsaun <iainsaun@desktop>
2025-04-18 08:54:19 +0100

    Overwrite WinchModTemplate

---
+++ WinchModTemplate/BigSpender.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace BigSpender
+{
+	public class BigSpender : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(BigSpender)} has loaded!");
+		}
+	}
+}
