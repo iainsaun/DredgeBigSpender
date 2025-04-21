a59408a91b6aa3feff3f1c705d58e71a641f06e5
iainsaun <iainsaun@desktop>
2025-04-21 20:21:08 +0100

    Trophy fish sell prices increased, dredge materials sell prices increased

--- BigSpender/Prices/Sell_Patch.cs
+++ BigSpender/Prices/Sell_Patch.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HarmonyLib;
+using JetBrains.Annotations;
 // Copied from github.com/Johncook2/Stress-and-Taxes
 // Only fish values reduced, all other item values the same or better, making dredging more lucrative
 namespace BigSpender.Prices.Patches
@@ -15,16 +16,17 @@ namespace BigSpender.Prices.Patches
         public static void GetItemValue(ref decimal __result, SpatialItemInstance itemInstance, ItemManager.BuySellMode mode, float sellValueModifier = 0.5f)
         {
             SpatialItemData itemData = itemInstance.GetItemData<SpatialItemData>();
+            //bool isTrophy = fishItemInstance.IsTrophySize();
 
             bool flag = mode != ItemManager.BuySellMode.SELL;
             if (!flag)
             {
                 __result *= (decimal)1.2;
-
                 if (itemData.itemSubtype == ItemSubtype.FISH)
                 {
-                    __result *= (decimal)0.1;
+                    __result *= (decimal)0.2;
                     FishItemData FishItemData = itemData as FishItemData;
+                    FishItemInstance FishItemInstance = itemInstance as FishItemInstance;
                     if (!FishItemData.day)
                     {
                         __result *= (decimal)2.0;
@@ -33,12 +35,21 @@ namespace BigSpender.Prices.Patches
                     {
                         __result *= (decimal)2.0;
                     }
+                    if (FishItemInstance.size >= 0.85)
+                    {
+                        __result += FishItemData.value * (decimal)0.4;
+                    }
+                }
+
+                if (itemData.itemSubtype == ItemSubtype.MATERIAL)
+                {
+                    __result *= (decimal)5.0;
                 }
 
             }
             if (flag)
             {
-                __result *= (decimal)1.1;
+                __result *= (decimal)1.0;
             }
         }
     }
